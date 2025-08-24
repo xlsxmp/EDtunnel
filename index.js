1d831a4a2ed3ba290908f67f5fa5b44d3c44bd7e
xlsxmp <xlsxmp@gmail.com>
2025-08-24 18:36:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ import { connect } from 'cloudflare:sockets';
  * User configuration and settings
  * Generate UUID: [Windows] Press "Win + R", input cmd and run: Powershell -NoExit -Command "[guid]::NewGuid()"
  */
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'b311c32c-15f6-4a83-8799-88f36bd9dfcb';
 
 /**
  * Array of proxy server addresses with ports
  * Format: ['hostname:port', 'hostname:port']
  */
-const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
+const proxyIPs = ['proxyip.fxxk.dedyn.io:443', 'bpb.yousef.isegaro.com:443'];
 
 // Randomly select a proxy server from the pool
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
